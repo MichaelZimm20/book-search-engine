1ce89aa36a1cb964b63f0aa940b0575d3afabaf7
DJMIKE20 <m.zimm20@gmail.com>
2022-12-03 14:45:27 -0500

    export typedefs and resolvers from schemas index.js

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import typeDefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// export typeDefs and resolvers using destructure object
+module.exports = { typeDefs, resolvers }
